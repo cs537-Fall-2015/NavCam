224cc55bf2b7ffc7c814909b725b2662eaf2447c
AhmedAlObaidi <ahmedalobaidi89@gmail.com>
2015-10-31 11:40:13 -0700

    Fix MyFrame class

--- workspace/CS537/src/webcamcapture/MyFrame.java
+++ workspace/CS537/src/webcamcapture/MyFrame.java
@@ -12,13 +12,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MyFrame extends JFrame {
-	
 
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MyFrame extends JFrame {
     private JPanel contentPane;
-    private static Timer timer;
-    private static MyFrame myFrame;
-    
 
   /**
   * Launch the application.
@@ -27,16 +30,8 @@ public class MyFrame extends JFrame {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                	
-                	
                     MyFrame frame = new MyFrame();
-                    
-                    
                     frame.setVisible(true);
-                   
-                    
-                    
-                    
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -49,23 +44,18 @@ public class MyFrame extends JFrame {
   */
     public MyFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 1000, 800);
+        setBounds(100, 100, 650, 490);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setBounds(100, 100, 700, 500);
         setContentPane(contentPane);
         contentPane.setLayout(null);
+        
         
         setLayout(new BorderLayout());
         JButton button = new JButton("SOUTH");
         add(button, BorderLayout.WEST);
-        
-        timer = new Timer();	// Timer helps get current frame of camera  
-		timer.scheduleAtFixedRate(new RepaintTask(), new Date(), 50);	// 50ms will trigger the timer
-		validate();
-		repaint();
   
-       // new MyThread().start();
+        new MyThread().start();
     }
  
     VideoCap videoCap = new VideoCap();
@@ -75,15 +65,14 @@ public class MyFrame extends JFrame {
         g.drawImage(videoCap.getOneFrame(), 0, 0, this);
     }
  
-    class RepaintTask extends TimerTask{
-    	public void run(){
-    		//System.out.println("Reporting from RepaintTask. I'm doing the work");
-    		myFrame.update();	// This will get the current frame of camera
-    		validate();
-    		repaint();
-    	}
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for (;;){
+                repaint();
+                try { Thread.sleep(30);
+                } catch (InterruptedException e) {    }
+            }  
+        } 
     }
-    
-    
-
 }
